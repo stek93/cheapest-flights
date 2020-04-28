c985e5565082622214590e6377654a723102dc0b
Stefan Kajkut <stefan.kajkut@codecentric.de>
2020-04-28 20:21:04 +0200

    Project initialization

---
+++ src/main/java/com/stek/flights/cheapestflights/CheapestFlightsApplication.java
@@ -0,0 +1,13 @@
+package com.stek.flights.cheapestflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapestFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheapestFlightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stek/flights/cheapestflights/CheapestFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stek.flights.cheapestflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheapestFlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
